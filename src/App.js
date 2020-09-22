cce0914bd69c708942490fcee1d9b5cd5fe5092c
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-09-22 14:10:56 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.currencies = ["AUD", "CAD", "CHF", "USD", "NZD", "EUR", "GBP"];
+    this.cached = [];
+    this.state = {
+      base: "USD",
+      target: "EUR",
+      value: 0,
+      converted: 0,
+    };
+  }
+
+  makeSelection = (event) => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      },
+      this.recalculate
+    );
+  };
+
+  changeValue = (event) => {
+    this.setState(
+      {
+        value: event.target.value,
+      },
+      this.recalculate
+    );
+  };
+
+  recalculate = () => {
+    const value = parseFloat(this.state.value);
+    if (isNaN(value)) {
+      return;
+    }
+    if (
+      this.cached[this.state.base] !== undefined &&
+      Date.now() - this.cached[this.state.base].timestamp < 10000 * 60
+    ) {
+      this.setState({
+        converted:
+          this.cached[this.state.base].rates[this.state.target] * value,
+      });
+    }
+    fetch(`https://api.exchangeratesapi.io/latest?base=${this.state.base}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.cached[this.state.base] = {
+          rates: data.rates,
+          timestamp: Date.now(),
+        };
+        this.setState({
+          converted: data.rates[this.state.target] * value,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <select
+            onChange={this.makeSelection}
+            name="base"
+            value={this.state.base}
+          >
+            {this.currencies.map((currency) => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          </select>
+          <input value={this.state.value} onChange={this.changeValue} />
+        </div>
+        <div>
+          <select
+            onChange={this.makeSelection}
+            name="target"
+            value={this.state.target}
+          >
+            {this.currencies.map((currency) => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          </select>
+          <input
+            disable={true}
+            value={
+              this.state.converted === null
+                ? "Calculating"
+                : this.state.converted
+            }
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
